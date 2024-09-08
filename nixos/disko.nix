558546fe077f3767384c6dc990e4f8b3884a926f
Elias Ainsworth <pilum-murialis.toge@proton.me>
2024-09-07 18:36:07 -0700

    added basic disko config

---
+++ nixos/disko.nix
@@ -0,0 +1,85 @@
+{
+  device ? throw "Set this to your disk device, e.g. /dev/sda",
+  hostname,
+  ...
+}: {
+  disko = {
+    devices = {
+      disk = {
+        main = {
+          inherit device;
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                name = "boot";
+                size = "1M";
+                type = "EF02";
+              };
+              esp = {
+                name = "ESP";
+                size = "500M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              swap = {
+                size = "6G";
+                content = {
+                  type = "swap";
+                  resumeDevice = true;
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "zroot";
+                };
+              };
+            };
+          };
+        };
+      };
+      zpool = {
+        zroot = {
+          type = "zpool";
+          rootFsOptions = {
+            canmount = "off";
+          };
+          datasets = {
+            nix = {
+              type = "zfs_fs";
+              mountpoint = "/nix";
+              options.mountpoint = "legacy";
+            };
+            persist = {
+              type = "zfs_fs";
+              mountpoint = "/persist";
+              options.mountpoint = "legacy";
+            };
+            persist-cache = {
+              type = "zfs_fs";
+              mountpoint = "/persist/cache";
+              options.mountpoint = "legacy";
+            };
+            tmp = {
+              type = "zfs_fs";
+              mountpoint = "/tmp";
+              options.mountpoint = "legacy";
+            };
+            disks = {
+              type = "zfs_fs";
+              mountpoint = "/disks";
+              options.mountpoint = "legacy";
+            };
+          };
+        };
+      };
+    };
+  };
+}
